b4d4614a9bfa0f312decefaba79ab9555ad780b1
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-06-20 11:31:01 +0100

    using github as soruce

--- ts-infra/lib/frontend-cd-stack.ts
+++ ts-infra/lib/frontend-cd-stack.ts
@@ -4,33 +4,43 @@ import * as codecommit from '@aws-cdk/aws-codecommit';
 import * as iam from "@aws-cdk/aws-iam"
 import {BuildSpec} from "@aws-cdk/aws-codebuild";
 import * as s3 from "@aws-cdk/aws-s3"
+import * as secretsmanager from "@aws-cdk/aws-secretsmanager"
 
 export class CodeBuildFrontend extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, bucket: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
-        const repository = codecommit.Repository.fromRepositoryName(this, 'repo', "messaging_serverless")
-        const source = codebuild.Source.codeCommit({repository: repository, branchOrRef: "ts"})
 
-        const frontendBucket = s3.Bucket.fromBucketName(this, 'bucket', bucket )
+        // github source
+        const gitHubSource = codebuild.Source.gitHub({
+            owner: 'manrodri',
+            repo: 'messaging_serverless',
+            webhook: true, // optional, default: true if `webhookFilters` were provided, false otherwise
+            webhookFilters: [
+                codebuild.FilterGroup
+                    .inEventOf(codebuild.EventAction.PUSH)
+                    .andBranchIs('ts')
+            ], // optional, by default all pushes and Pull Requests will trigger a build
+        });
+
+        const frontendBucket = s3.Bucket.fromBucketName(this, 'bucket', bucket)
 
         const buildProject = new codebuild.Project(this, 'MyFirstCodeCommitProject', {
-            source: source,
+            source: gitHubSource,
             buildSpec: BuildSpec.fromSourceFilename("ts-infra/buildspec.yaml"),
             projectName: "frontendCodeBuildJob",
             environment: {
                 buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
                 environmentVariables: {
-                  HOSTING_BUCKET: {
-                      value: frontendBucket.bucketName,
-                      type: codebuild.BuildEnvironmentVariableType.PLAINTEXT
-                  }
+                    HOSTING_BUCKET: {
+                        value: frontendBucket.bucketName,
+                        type: codebuild.BuildEnvironmentVariableType.PLAINTEXT
+                    }
                 }
             }
         });
 
-        buildProject.role?.addToPolicy(new iam.PolicyStatement({actions:["s3:*"], resources:["*"]}))
+        buildProject.role?.addToPolicy(new iam.PolicyStatement({actions: ["s3:*"], resources: ["*"]}))
 
 
     }
