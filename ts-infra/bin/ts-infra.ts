ed65c1c8bf464394dbf2c85a0c333718bf979064
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-07-05 06:50:30 +0100

    clean up ts-infra.ts

--- ts-infra/bin/ts-infra.ts
+++ ts-infra/bin/ts-infra.ts
@@ -2,15 +2,14 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { S3Stack } from '../lib/s3-stack';
-import {CodeBuildFrontend} from "../lib/frontend-cd-stack";
 import {LambdaStack} from "../lib/lambda-stack";
-import {backendCpStack} from "../lib/backendCpStack";
 import {messagingAppPipelineStack} from "../lib/cdkpipeline-definition-stack";
 
 const app = new cdk.App();
 
-
-const environment = { account: '423754860743', region: 'eu-west-1' }
+const region = process.env.CDK_DEFAULT_REGION || 'eu-west-1'
+const account = process.env.CDK_DEFAULT_ACCOUNT || '423754860743'
+const environment = { account: account, region: region }
 
 new S3Stack(app, 'S3Stack',{env:environment});
 const backendStack = new LambdaStack(app, 'backendStack',cdk.Fn.importValue("frontendBucket"), {env: environment})
@@ -18,9 +17,5 @@ new messagingAppPipelineStack(app, 'pipelineStack', cdk.Fn.importValue("frontend
     env: environment
 })
 
-// new  backendCpStack(app, 'backendPipelineStack', {env: environment})
-// new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
-//   env: environment,
-// });
 
 app.synth()
